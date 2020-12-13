9cbef07fa8552562ce81cd469c528b232092f77e
Nguyen Anh Tuan <michigo2802@gmail.com>
2020-12-13 10:24:38 +0700

    Định dạng lại code bên Server và Client.

--- client/Client.java
+++ client/Client.java
@@ -1,64 +1,116 @@
+/*
+Ho va ten: Nguyen Anh Tuan
+MSSV: 18021376
+Mo ta: file Client
+*/
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.*;
 import java.net.UnknownHostException;
+import java.util.Observable;
+import java.util.Observer;
 import java.util.Scanner;
 
 import java.net.Socket;
 
-public class Client {
-    private static final int DEFAULT_PORT = 8080;
+public class Client{
+
     private Socket socket = null;
     private Scanner scn = null;
     private DataInputStream dis = null;
     private DataOutputStream dos = null;
+    private ClientGUI clientGUI = null;
+    private String host;
+    private int port;
 
-    public Client(String address, int port) {
+    public Client(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public Client(String host, int port, ClientGUI clientGUI) {
+        this.host = host;
+        this.port = port;
+        this.clientGUI = clientGUI;
+    }
+
+    public boolean init() {
         try {
-            socket = new Socket(address, port);
-            scn = new Scanner(System.in);
+            socket = new Socket(host, port);
+            String msg = "Connection accepted " + socket.getInetAddress() + ":" + socket.getPort();
+            display(msg);
             dis = new DataInputStream(socket.getInputStream());
             dos = new DataOutputStream(socket.getOutputStream());
+            send(socket.getInetAddress() + ":" + socket.getPort() + " connected");
 
             Thread receiveThread = new Thread(){
                 @Override
                 public void run() {
                     try {
                         String received;
-                        while(!(received = dis.readUTF()).trim().isEmpty()) {
-                            System.out.print(received);
-                            if (received.equals("Bye")) {
-                                socket.close();
-                                break;
+                        while(true) {
+                            received = dis.readUTF().trim();
+
+                            if (received.indexOf("online") >= 0)
+                                displayUser(received);
+                            else {
+                                String words[] = received.split("\\s");
+                                if (received.startsWith("Download")) {
+                                    downloadFile("./storage/client/" + words[2]);
+                                }
+                                display(received);
+                                if (received.equals("Bye User")) {
+                                    close();
+                                    break;
+                                }
                             }
                         }
                     } catch (IOException ioe) {
-                        System.out.println(ioe);
+                        display("Server has close the connect " + ioe);
                     }
                 }
             };
             receiveThread.start();
-
-            String tosend;
-            while(!(tosend = scn.nextLine()).trim().isEmpty()) {
-                dos.writeUTF(tosend);
-                if(tosend.equals("/quit"))
-                {
-                    //System.out.println("Closing this connection : " + socket);
-                    System.out.println("Connection closed");
-                    break;
-                }
-            }
-
         } catch (UnknownHostException uhe) {
-            System.out.println(uhe);
+            display("Fault: " + uhe);
+            return false;
         } catch (IOException ioe) {
-            System.out.println(ioe);
+            display("Fault: " + ioe);
+            return false;
         }
+        return true;
+    }
 
+    public void display(String msg) {
+        if (clientGUI != null)
+            clientGUI.appendRoom(msg + "\n");
+    }
+
+    public void displayUser(String msg) {
+        if (clientGUI != null)
+            clientGUI.appendUser(msg + "\n");
+    }
+
+    public void send(String tosend) {
         try {
-            scn.close();
+            dos.writeUTF(tosend);
+        } catch (IOException ioe) {
+            display("Fault: " + ioe);
+        }
+    }
+
+    public void close(){
+        try {
             dis.close();
             dos.close();
-            //socket.close();
+            socket.close();
+            if (clientGUI != null)
+                clientGUI.connectionFailed();
         }
         catch (IOException ioe)
         {
@@ -66,15 +118,55 @@ public class Client {
         }
     }
 
-    public static void main(String[] args) {
-        System.out.print("Input address to connecting with Server: ");
-        String addr = "";
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        try {
-            addr = reader.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public void sendFile(String filename) throws IOException {
+        File infile = new File(filename);
+        FileInputStream fis = new FileInputStream(infile);
+        BufferedInputStream bis = new BufferedInputStream(fis);
+        byte[] contents;
+        long fileLength = infile.length();
+        long current = 0;
+        dos.writeLong(fileLength);
+        while(current != fileLength)
+        {
+            int size = 1024;
+            if (fileLength - current >= size)
+                current += size;
+            else {
+                size = (int)(fileLength - current);
+                current = fileLength;
+            }
+            contents = new byte[size];
+            bis.read(contents, 0, size);
+
+            dos.write(contents);
+
         }
-        Client client = new Client(addr, DEFAULT_PORT);
+        dos.flush();
+    }
+
+    public void downloadFile(String filename) throws IOException {
+        FileOutputStream fos    = new FileOutputStream(filename);
+        BufferedOutputStream bos= new BufferedOutputStream(fos);
+        byte[] contents         = new byte[1024];
+        long fileLength         = dis.readLong();
+        int byteRead            = 0;
+        int size                = 0;
+        while(size < fileLength) {
+            byteRead = dis.read(contents);
+            bos.write(contents, 0, byteRead);
+            size += byteRead;
+        }
+
+        bos.flush();
+        //fos.close();
+    }
+
+    // --- main ---
+    public static void main(String[] args) {
+        final int DEFAULT_PORT = 8080;
+        String host = "localhost";
+        Client client = new Client(host, DEFAULT_PORT);
+
+        client.init();
     }
 }
--- server/Server.java
+++ server/Server.java
@@ -1,24 +1,54 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class Server {
+    private int port;
     private ServerSocket serverSocket = null;
     private Socket clientSocket = null;
     private final int maxClients = 10;
     private ClientHandler clients[] = new ClientHandler[maxClients];
+    private boolean servercontinue = false;
+    private ServerGUI serverGUI = null;
+    private SimpleDateFormat sdf = null;
 
     public Server(int port) {
+        this.port = port;
+    }
+
+    public Server(int port, ServerGUI serverGUI) {
+        this.port = port;
+        this.serverGUI = serverGUI;
+        this.sdf = new SimpleDateFormat("HH:mm:ss");
+    }
+
+    public ServerGUI getServerGUI() {
+        return serverGUI;
+    }
+
+    public SimpleDateFormat getSdf() {
+        return sdf;
+    }
+
+    public void init() {
+        servercontinue = true;
         try {
             serverSocket = new ServerSocket(port);
             System.out.println("Server start");
-            //System.out.println("Waiting a connection ...");
-            while(true) {
+
+            while(servercontinue) {
+                displayEvent("Waiting connections on port " + this.port + "...");
                 clientSocket = serverSocket.accept();
+
+                // stop waiting clients
+                if (!servercontinue) break;
+
                 int i;
                 for (i = 0; i<maxClients; i++) {
                     if (clients[i] == null) {
-                        clients[i] = new ClientHandler(clientSocket, clients);
+                        clients[i] = new ClientHandler(clientSocket, clients, this);
                         clients[i].start();
                         break;
                     }
@@ -36,6 +66,22 @@ public class Server {
         }
     }
 
+    protected void stop() {
+        servercontinue = false;
+        try {
+            new Socket("localhost", port);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void displayEvent(String msg) {
+        String mess = "[" + sdf.format(Calendar.getInstance().getTime()) + "] " + msg;
+        if (serverGUI != null)
+            serverGUI.appendEvent(mess + "\n");
+    }
+
     public static void main(String[] args) {
         int portNumber = 8080;
         if (args.length < 1) {
@@ -46,148 +92,7 @@ public class Server {
         }
 
         Server server = new Server(portNumber);
+        server.init();
     }
 }
 
-class ClientHandler extends Thread {
-    private String clientName = null;
-    private Socket clientSocket = null;
-    private DataInputStream is = null;
-    private DataOutputStream os = null;
-    private int maxClients;
-    private final ClientHandler []clients;
-
-    public ClientHandler(Socket clientSocket, ClientHandler[] clients)
-    {
-        this.clientSocket = clientSocket;
-        this.clients = clients;
-        maxClients = clients.length;
-    }
-
-    @Override
-    public void run() {
-        int maxClients = this.maxClients;
-        ClientHandler[] clients = this.clients;
-
-        try {
-            is = new DataInputStream(clientSocket.getInputStream());
-            os = new DataOutputStream(clientSocket.getOutputStream());
-
-            // enter name not have '@' character
-            String name;
-            while(true) {
-                os.writeUTF("Enter your name: ");
-                try {
-                    name = is.readUTF().trim();
-                    if (name.indexOf('@') == -1) {
-                        break;
-                    } else {
-                        os.writeUTF("The name should not contain '@' character.\n");
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            //Welcome
-            os.writeUTF("Welcome " + name + " to our chat room. To leave enter /quit in a new line.\n");
-
-            synchronized (this) {
-                for (int i=0; i<maxClients; i++) {
-                    if (clients[i] != null && clients[i] == this) {
-                        clientName = "@" + name;
-                        break;
-                    }
-                }
-
-                // announce the list of users who are online
-                String notification = "List User Online:\n";
-                for (int i=0; i<maxClients; i++) {
-                    if (clients[i] != null && clients[i] != this) {
-                        notification += (clients[i].clientName.substring(1) + " online\n");
-                    }
-                    if (i == (maxClients - 1)) {
-                        if (!notification.equals("List User Online:\n")) {
-                            this.os.writeUTF(notification);
-                        } else {
-                            this.os.writeUTF(notification + "No user online\n");
-                        }
-                    }
-                }
-
-                // notice that new users are online
-                for (int i=0; i<maxClients; i++) {
-                    if (clients[i] != null && clients[i] != this) {
-                        clients[i].os.writeUTF("--- A new user " + name + " entered the chat room ---\n");
-                    }
-                }
-            }
-
-            // Start the conversation
-            String received = "";
-            while(!(received = is.readUTF()).trim().isEmpty()) {
-                try {
-                    //received = is.readUTF();
-                    if (received.startsWith("/quit")) break;
-                    // if the message is private sent it to the given client.
-                    if (received.startsWith("@")) {
-                        String[] words = received.split("\\s", 2);
-                        if (words.length > 1 && words[1] != null) {
-                            words[1] = words[1].trim();
-                            if (!words[1].isEmpty()) {
-                                synchronized (this) {
-                                    for (int i=0; i<maxClients; i++) {
-                                        if (clients[i] != null && clients[i] != this
-                                                && clients[i].clientName != null
-                                                && clients[i].clientName.equals(words[0])) {
-                                            clients[i].os.writeUTF("[" + name + " to you] " + words[1] + "\n");
-                                            //this.os.writeUTF(">" + name + ">" + words[1]);
-                                            break;
-                                        }
-                                        if (i == (maxClients - 1)) {
-                                            this.os.writeUTF(words[0] + " not online || " + words[0] + " not exists\n");
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    } else {
-                        // the message is public, broadcast it to all clients
-                        synchronized (this) {
-                            for (int i = 0; i < maxClients; i++) {
-                                if (clients[i] != null && clients[i].clientName != null && clients[i] != this) {
-                                    clients[i].os.writeUTF("[" + name + "] " + received + "\n");
-                                }
-                            }
-                        }
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            synchronized (this) {
-                for (int i=0; i<maxClients; i++) {
-                    if (clients[i] != null && clients[i] != this && clients[i].clientName != null) {
-                        clients[i].os.writeUTF("--- The user " + name + " is leaving the chat room! ---\n");
-                    }
-                }
-            }
-            os.writeUTF("Bye\n");
-
-            // clean up
-            synchronized (this) {
-                for (int i=0; i<maxClients; i++) {
-                    if (clients[i] == this) {
-                        clients[i] = null;
-                    }
-                }
-            }
-
-            is.close();
-            os.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
